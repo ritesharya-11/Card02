f2a62694b0378440d537b98d410d2b3545941cb6
RITESH KUMAR <ritesharya9955@gmail.com>
2026-01-30 23:40:58 +0530

    some thin

--- src/components/Section1/RightCard.jsx
+++ src/components/Section1/RightCard.jsx
@@ -3,7 +3,7 @@ import RightCardContent from './RightCardContent'
 
 const RightCard = () => {
   return (
-    <div className='h-full shrink-0 overflow-hidden relative w-80  rounded-4xl'>
+    // <div className='h-full shrink-0 overflow-hidden relative w-80  rounded-4xl'>
       <img className='h-full w-full object-cover' src="https://plus.unsplash.com/premium_photo-1661630621969-6d9faac03f9f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D" alt="" />
       <RightCardContent/>
     </div>
